258cdef6d55bc9050128b64d6448ebc522e84951
bemkap <bemkap@gmail.com>
2016-10-07 20:16:51 -0300

    chip8

--- chip8.c
+++ chip8.c
@@ -1,12 +1,6 @@
 #include"chip8.h"
-#include<time.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 
-struct CHIP8 M;
-
-F(x00e0,for(byte i=0;i<32*64;++i) SCR[i/64][i%64]=0;PC+=2)		//CLS
+F(x00e0,for(word i=0;i<32*64;++i) SCR[i]=0;PC+=2)        		//CLS
 F(x00ee,PC=STK[ST--])							//RET
 F(x1nnn,PC=nnn)								//JP   addr
 F(x2nnn,STK[++SP]=PC;PC=nnn)						//CALL addr
@@ -29,14 +23,14 @@ F(xannn,I=nnn;PC+=2)							//LD   I, addr
 F(xbnnn,PC=nnn+V0)							//JP   V0, addr
 F(xcxkk,srand(time(NULL));Vx=(rand()%256)&kk;PC+=2)			//RND  Vx, byte
 F(xdxyn,VF=0;for(byte i=0;i<n;++i) for(byte j=0;j<8;++j){		//DRW  Vx, Vy, nibble
-      if(SCR[Vx+i][(Vy+j)%64]+(MEM[I+i]>>j)&1==2)VF=1;
-      SCR[Vx+i][(Vy+j)%64]^=(MEM[I+i]>>j)&1;
+      if(SCR[Vx+i*64+(Vy+j)%64]+(MEM[I+i]>>j)&1==2) VF=1;
+      SCR[(Vx+i)*64+(Vy+j)%64]^=(MEM[I+i]>>j)&1;
     }
   PC+=2)
 F(xex9e,PC+=2*(KB&(1<<Vx));PC+=2)					//SKP  Vx
 F(xexa1,PC+=2*(0==KB&(1<<Vx));PC+=2)					//SKPN Vx
 F(xfx07,Vx=DT;PC+=2)							//LD   Vx, DT
-F(xfx0a,Vx=MAP[getchar()];PC+=2)					//LD   Vx, K
+F(xfx0a,Vx=getchar();PC+=2)            					//LD   Vx, K
 F(xfx15,DT=Vx;PC+=2)							//LD   DT, Vx
 F(xfx18,ST=Vx;PC+=2)							//LD   ST, Vx
 F(xfx1e,I+=Vx;PC+=2)							//ADD  I, Vx
@@ -45,33 +39,62 @@ F(xfx33,MEM[I]=Vx/100;MEM[I+1]=(Vx)%100/10;MEM[I+2]=Vx%10;PC+=2)	//LD   B, Vx
 F(xfx55,for(byte i=0;i<Vx;++i) MEM[I+i]=V[i];PC+=2)			//LD   [I], Vx
 F(xfx65,for(byte i=0;i<Vx;++i) V[i]=MEM[I+i];PC+=2)			//LD   Vx, [I]
 
-void f0(word opcode){(inst[]){f00e0,f00ee}[opcode>>1&1](opcode);}
-void f8(word opcode){(inst[]){f8xy0,f8xy1,f8xy2,f8xy3,f8xy4,f8xy5,f8xy6,f8xy7,0,0,0,0,0,0,f8xye,0}[opcode&0xf](opcode);}
-void fe(word opcode){(inst[]){fex9e,fexa1}[opcode&1](opcode);}
-void ff(word opcode){inst H[256];
-  H[0x07]=ffx07;H[0x0a]=ffx0a;H[0x15]=ffx15;H[0x18]=ffx18;H[0x1e]=ffx1e;
-  H[0x29]=ffx29;H[0x33]=ffx33;H[0x55]=ffx55;H[0x65]=ffx65;
-  H[opcode&0xff](opcode);
+void f0(struct CHIP8*M,word oc){(inst[]){x00e0,x00ee}[oc>>1&1](M,oc);}
+void f8(struct CHIP8*M,word oc){(inst[]){x8xy0,x8xy1,x8xy2,x8xy3,x8xy4,x8xy5,x8xy6,x8xy7,0,0,0,0,0,0,x8xye,0}[oc&0xf](M,oc);}
+void fe(struct CHIP8*M,word oc){(inst[]){xex9e,xexa1}[oc&1](M,oc);}
+void ff(struct CHIP8*M,word oc){inst H[256];
+  H[0x07]=xfx07;H[0x0a]=xfx0a;H[0x15]=xfx15;H[0x18]=xfx18;H[0x1e]=xfx1e;
+  H[0x29]=xfx29;H[0x33]=xfx33;H[0x55]=xfx55;H[0x65]=xfx65;
+  H[oc&0xff](M,oc);
 }
 
 byte charset[5*16]={
-  0xF0,0x90,0x90,0x90,0xF0,  0x20,0x60,0x20,0x20,0x70,  0xF0,0x10,0xF0,0x80,0xF0,  0xF0,0x10,0xF0,0x10,0xF0,
-  0x90,0x90,0xF0,0x10,0x10,  0xF0,0x80,0xF0,0x10,0xF0,  0xF0,0x80,0xF0,0x90,0xF0,  0xF0,0x10,0x20,0x40,0x40,
-  0xF0,0x90,0xF0,0x90,0xF0,  0xF0,0x90,0xF0,0x10,0xF0,  0xF0,0x90,0xF0,0x90,0x90,  0xE0,0x90,0xE0,0x90,0xE0,
-  0xF0,0x80,0x80,0x80,0xF0,  0xE0,0x90,0x90,0x90,0xE0,  0xF0,0x80,0xF0,0x80,0xF0,  0xF0,0x80,0xF0,0x80,0x80
+  0xf0,0x90,0x90,0x90,0xf0,  0x20,0x60,0x20,0x20,0x70,  0xf0,0x10,0xf0,0x80,0xf0,  0xf0,0x10,0xf0,0x10,0xf0,
+  0x90,0x90,0xf0,0x10,0x10,  0xf0,0x80,0xf0,0x10,0xf0,  0xf0,0x80,0xf0,0x90,0xf0,  0xf0,0x10,0x20,0x40,0x40,
+  0xf0,0x90,0xf0,0x90,0xf0,  0xf0,0x90,0xf0,0x10,0xf0,  0xf0,0x90,0xf0,0x90,0x90,  0xe0,0x90,0xe0,0x90,0xe0,
+  0xf0,0x80,0x80,0x80,0xf0,  0xe0,0x90,0x90,0x90,0xe0,  0xf0,0x80,0xf0,0x80,0xf0,  0xf0,0x80,0xf0,0x80,0x80
 };
 
-int main(int argc,char*argv[]){
-  inst OP[16]={f0,f1nnn,f2nnn,f3xkk,f4xkk,f5xy0,f6xkk,f7xkk,f8,f9xy0,fannn,fbnnn,fcxkk,fdxyn,fe};
-  FILE*in=fopen(argv[1],"rb");
-  fread(M.MEM+0x200,1,4096-0x200,in);
+void init(struct CHIP8*M,char*fp){
+  FILE*in=fopen(fp,"rb");
+  fread(M->MEM+0x200,1,4096-0x200,in);
   fclose(in);
-  memcpy(M.MEM,charset,5*16);
-  M.PC=0x200;
-  M.I=M.SP=0;
+  memcpy(M->MEM,charset,5*16);
+  M->PC=0x200;
+  M->I=M->SP=0;
+}
+
+SDL_Window*w;
+SDL_Surface*s;
+
+void init_sdl(){
+  SDL_Init(SDL_INIT_VIDEO);
+  w=SDL_CreateWindow("",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,320,SDL_WINDOW_SHOWN);
+  s=SDL_GetWindowSurface(w);
+}
+
+void close_sdl(){
+  SDL_FreeSurface(s);
+  SDL_DestroyWindow(w);
+  SDL_Quit();
+}
+
+int main(int argc,char*argv[]){
+  inst OP[16]={f0,x1nnn,x2nnn,x3xkk,x4xkk,x5xy0,x6xkk,x7xkk,f8,x9xy0,xannn,xbnnn,xcxkk,xdxyn,fe};
+  struct CHIP8 M;
+  init(&M,argv[1]);
+  init_sdl();
   for(;;){
-    word i=*(word*)(M.MEM+0x200+i);
-    OP[i>>12&0xf](i);
+    //exec
+    word i=*(word*)(M.MEM+0x200+M.PC);
+    OP[i>>12&0xf](&M,i);
+    //draw
+    for(word i=0;i<32*64;++i) SDL_FillRect(s,&(SDL_Rect){10*i/64,10*i%64,10,10},M.SCR[i]?WHITE:BLACK);
+    SDL_UpdateWindowSurface(w);
+    //update
+    M.DT=(--M.DT<0)?0:M.DT;
+    M.ST=(--M.ST<0)?0:M.ST;
   }
+  close_sdl();
   return 0;
 }
--- chip8.h
+++ chip8.h
@@ -1,20 +1,30 @@
 #pragma once
 
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<SDL2/SDL.h>
+
+#define WHITE 0xffffff
+#define BLACK 0x000000
+
 typedef unsigned char byte;
 typedef unsigned short word;
-typedef void(*inst)(word);
 
 struct CHIP8 {
-  byte DT,ST,SP,MEM[4096],SCR[32][64],V[16];
+  byte DT,ST,SP,MEM[4096],SCR[32*64],V[16];
   word I,PC,KB,STK[16];
 };
 
-extern struct CHIP8 M;
+typedef void(*inst)(struct CHIP8*,word);
 
-#define F(id,body) void id(word oc){			\
-    byte Vx=M.V[oc>>8&0xf],Vy=M.V[oc>>4&0xf];		\
-    byte n=oc&0xf,kk=oc&0xff,nnn=oc&0xfff;		\
-    byte DT=M.DT,ST=M.ST,*MEM=M.MEM,**SCR=M.SCR;	\
-    word I=M.I,PC=M.PC,KB=M.KB,*STK=M.STK;		\
-    do{body;}while(0);					\
+#define F(id,body) void id(struct CHIP8*M,word oc){        \
+    byte Vx=M->V[oc>>8&0xf],Vy=M->V[oc>>4&0xf];            \
+    byte V0=M->V[0x0],VF=M->V[0xf];                        \
+    byte n=oc&0xf,kk=oc&0xff,nnn=oc&0xfff;                 \
+    byte DT=M->DT,ST=M->ST,SP=M->SP;                       \
+    byte *MEM=M->MEM,*SCR=M->SCR,*V=M->V;                  \
+    word I=M->I,PC=M->PC,KB=M->KB,*STK=M->STK;             \
+    do{body;}while(0);                                     \
   }
