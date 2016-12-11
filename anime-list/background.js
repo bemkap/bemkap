a9ee9d76dd913250656e0363087e1bdfbd055ae9
bemkap <bemkap@gmail.com>
2016-12-11 08:42:05 -0300

    anime-list-0.2

--- anime-list/background.js
+++ anime-list/background.js
@@ -5,7 +5,7 @@ chrome.commands.onCommand.addListener(function(command){
 	    var idx0=tabs[0].url.lastIndexOf("/");
 	    var idx1=tabs[0].url.lastIndexOf("-");
 	    var anime_id="anime-list."+tabs[0].url.slice(idx0+1,idx1);
-            chrome.cookies.set({url:"http://animeflv.net",name:anime_id,value:tabs[0].url});
+            chrome.cookies.set({url:"http://animeflv.net",name:anime_id,value:tabs[0].url,persistent:true});
         });
     }
 });
--- anime-list/popup.js
+++ anime-list/popup.js
@@ -1,19 +1,17 @@
 window.onload=function(){
-    var table=document.getElementById('last');
+    var list=document.getElementById('last');
     chrome.cookies.getAll({url:"http://animeflv.net"},function(cookies){
 	for(var i=0; i<cookies.length; ++i){
-	    var context_name=cookies[i].name.split(".");
+            var context_name=cookies[i].name.split(".");
 	    if(context_name[0]=="anime-list"){
-		var tr=document.createElement('tr');
-		var th1=document.createElement('th');
+		var li=document.createElement('li');
 		var a=document.createElement('a');
 		var idx=cookies[i].value.lastIndexOf("-")+1;
-		//a.textContent=context_name[1];
-		a.innerHTML=context_name[1]+"<br/>&emsp;&emsp;Capítulo "+cookies[i].value.slice(idx);
+                var name=context_name[1].replace(/-/g," ");
+		a.innerHTML=name+"<br/><small>&ensp;capítulo "+cookies[i].value.slice(idx)+"</small>";
 		a.setAttribute("href",cookies[i].value);
-		th1.appendChild(a);
-		tr.appendChild(th1);
-		table.appendChild(tr);
+		li.appendChild(a);
+		list.appendChild(li);
 	    }
 	}
     });
