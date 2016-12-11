8a9c6f12aa46398bd21076ffd78b7f32cec10324
bemkap <bemkap@gmail.com>
2016-12-11 00:35:57 -0300

    anime-list

--- anime-list/background.js
+++ anime-list/background.js
@@ -2,15 +2,10 @@ chrome.commands.onCommand.addListener(function(command){
     switch(command){
     case "save-anime":
 	chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-	    anime_id=tabs[0].url.split("/")[4];
-            chrome.cookies.get({url:tabs[0].url,name:anime_id.toString()},function(cookie){
-                if(cookie==null)
-                    chrome.cookies.set({url:tabs[0].url,name:anime_id.toString()});
-                else
-                    alert(anime_id.toString());
-                
-            //chrome.storage.local.set({anime_id.toString():tabs[0].url});
-	    });
+	    var idx0=tabs[0].url.lastIndexOf("/");
+	    var idx1=tabs[0].url.lastIndexOf("-");
+	    var anime_id="anime-list."+tabs[0].url.slice(idx0+1,idx1);
+            chrome.cookies.set({url:"http://animeflv.net",name:anime_id,value:tabs[0].url});
         });
     }
 });
--- anime-list/popup.js
+++ anime-list/popup.js
@@ -1,9 +1,20 @@
 window.onload=function(){
-    var ul=document.getElementById('last');
-    chrome.storage.local.get('animes',function(animes){
-	for(var i=0; i<animes.length; ++i){
-	    var li=document.createElement('li');
-	    li.textContent=animes[i];
-	    li.value=animes[i];
-	    ul.appendChild(li);
-	}});}
+    var table=document.getElementById('last');
+    chrome.cookies.getAll({url:"http://animeflv.net"},function(cookies){
+	for(var i=0; i<cookies.length; ++i){
+	    var context_name=cookies[i].name.split(".");
+	    if(context_name[0]=="anime-list"){
+		var tr=document.createElement('tr');
+		var th1=document.createElement('th');
+		var a=document.createElement('a');
+		var idx=cookies[i].value.lastIndexOf("-")+1;
+		//a.textContent=context_name[1];
+		a.innerHTML=context_name[1]+"<br/>&emsp;&emsp;Capítulo "+cookies[i].value.slice(idx);
+		a.setAttribute("href",cookies[i].value);
+		th1.appendChild(a);
+		tr.appendChild(th1);
+		table.appendChild(tr);
+	    }
+	}
+    });
+};
