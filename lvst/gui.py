23572412240c10a555dd08d4135ca16f856eb3bd
bemkap <bemkap@gmail.com>
2018-09-09 20:41:25 -0300

    no me olvides

--- lvst/gui.py
+++ lvst/gui.py
@@ -1,6 +1,5 @@
-import tkinter as tk
+import Tkinter as tk
 import json
-import ven
 import threading
 from ven import db
 
@@ -10,30 +9,40 @@ class gui:
         self.lab=[tk.Label(self.root),tk.Label(self.root)]
         self.lst=[tk.Listbox(self.root),tk.Listbox(self.root)]
         self.scr=[tk.Scrollbar(self.root),tk.Scrollbar(self.root)]
-        self.stt=tk.Label(self.root,anchor=tk.W)
-        self.db=db()
+        self.stt=tk.Listbox(self.root)
+        self.n=0
+        self.db=db(self)
         self.fill()
-        for i in [0,1]:
+        for i in xrange(2):
             self.lst[i].config(yscrollcommand=self.scr[i].set)
             self.lab[i].grid(row=0,column=i*2)
             self.lst[i].grid(row=1,column=i*2)
             self.scr[i].config(command=self.lst[i].yview)
             self.scr[i].grid(row=1,column=1+i*2,sticky=tk.N+tk.S)
-        self.stt.grid(row=2,column=0,columnspan=4,sticky=tk.W)
-        self.lst[1].bind("<Double-Button-1>",self.sel0)
+        self.stt.grid(row=2,column=0,columnspan=4,sticky=tk.W+tk.E)
+        self.lst[1].bind("<Double-Button-1>",self.begin_dl)
+        self.root.protocol("WM_DELETE_WINDOW",self.close)
         self.root.mainloop()
     def fill(self):
-        for i in [0,1]: self.lst[i].delete(0,tk.END)
+        for l in self.lst: l.delete(0,tk.END)
         for i in sorted(self.db.downloaded()): self.lst[0].insert(tk.END,i)
         for i in sorted(self.db.left()): self.lst[1].insert(tk.END,i)
-        for l,i in zip(["downloaded({0})","left({0})"],[0,1]):
-            self.lab[i].config(text=l.format(self.lst[i].size()))
-    def sel0(self,event):
-        t="downloading {0}...".format(self.lst[1].get(tk.ACTIVE))
-        self.stt.config(text=t)
-        t=threading.Thread(target=self.db.download_one,args=(self.lst[1].get(tk.ACTIVE),))
-        t.start()
-        self.stt.config(text=self.stt.cget("text")+"ok")
+        for i,l in enumerate(["downloaded(%d)","left(%d)"]):
+            self.lab[i].config(text=l%self.lst[i].size())
+    def act(self,k,n):
+        s=self.stt.get(n)
+        self.stt.itemconfig(n,text="%s %s"%(s,k))
+        # self.stt.config(text="%s %s"%(self.lst[1].get(tk.ACTIVE),k))
+    def finish(self):
+        self.db.set(self.lst[1].get(tk.ACTIVE))
         self.fill()
+    def close(self):
+        self.db.close()
+        self.root.destroy()
+    def begin_dl(self,event):
+        self.stt.insert(tk.END,"%s"%self.lst[1].get(tk.ACTIVE))
+        t=threading.Thread(target=self.db.download,args=(self.lst[1].get(tk.ACTIVE),self.n))
+        self.n+=1
+        t.start()
 
-G=gui()
+gui()
--- lvst/ven.py
+++ lvst/ven.py
@@ -1,34 +1,33 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
+import BeautifulSoup
+from urllib2 import urlopen
 import sys
 import json
+from math import ceil
 
 class db:
-    def __init__(self):
+    def __init__(self,listener):
+        self.listener=listener
+        self.CHUNK=4096*16
         with open("db.json","r") as fd:
             self.dic=json.load(fd)
     def record(self,r1,r2):
-        url="http://venganzasdelpasado.com.ar/posts/page/{0}"
+        url="http://venganzasdelpasado.com.ar/posts/page/%d"
         for n in xrange(r1,r2+1):
-            p=urlopen(url.format(n)).read()
+            p=urlopen(url%n).read()
             t=BeautifulSoup.BeautifulSoup(p,"html.parser")
             for l in t.findAll("source"):
                 u=l.get("src")
                 self.dic.setdefault(u[-14:],{"name":u,"downloaded":False})
-    def download(self,n):
-        for k in dic:
-            self.download_one(k)
-            n=n-1
-            if n<=0: break
-    def download_one(self,k):
+    def download(self,k,n):
         if not self.dic[k]["downloaded"]:
             mp3=urlopen(self.dic[k]["name"])
-            size=mp3.info()["Content-Length"]
+            size=float(mp3.info()["Content-Length"])
             with open(k,"wb") as fd:
-                while True:
-                    d=mp3.read(size/50)
-                    if not d: break
-                    else: fd.write(d)
+                for i in xrange(int(ceil(size/self.CHUNK))):
+                    d=mp3.read(self.CHUNK)
+                    fd.write(d)
+                    self.listener.act("   %.2fMB/%.2fMB"%(float(i*self.CHUNK)/1000000,size/1000000),n)
+            self.listener.finish()
     def set(self,k):
         self.dic[k]["downloaded"]=True
     def downloaded(self):
