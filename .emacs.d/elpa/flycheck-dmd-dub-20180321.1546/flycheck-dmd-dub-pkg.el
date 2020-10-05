c4e77fde953aa636903beb9821b4ad88d793a0b2
bemkap <bemkap@gmail.com>
2020-10-04 21:48:33 -0300

    mono

---
+++ .emacs.d/elpa/flycheck-dmd-dub-20180321.1546/flycheck-dmd-dub-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-dmd-dub" "20180321.1546" "Sets flycheck-dmd-include-paths from dub package information" '((flycheck "0.24") (f "0.18.2")) :commit "995132ced9c0e4464a36c4435ae8248c23fc0511" :keywords '("languages") :url "http://github.com/atilaneves/flycheck-dmd-dub")
---
+++ .emacs.d/elpa/pretty-symbols-20140814.259/pretty-symbols-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pretty-symbols" "20140814.259" "Draw tokens as Unicode glyphs." 'nil :url "http://github.com/drothlis/pretty-symbols" :keywords '("faces"))
